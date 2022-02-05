ae2c623ac81dbb788f4457d7a0749989cbf8b3e3
Jim Whiteford <james.whiteford@students.plymouth.ac.uk>
2022-02-05 19:59:31 +0000

    Successful conection with firbase, add, update and delete

---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firbase:{
+    apiKey: "AIzaSyDBM5AiY-7Ph0a36h4jZuDd10U4UtN_RSI",
+    authDomain: "devdactic-54be2.firebaseapp.com",
+    projectId: "devdactic-54be2",
+    storageBucket: "devdactic-54be2.appspot.com",
+    messagingSenderId: "998534438559",
+    appId: "1:998534438559:web:24dcce8064dca6da590caa"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
